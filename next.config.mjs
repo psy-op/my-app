adf184121750f5043c5dbad188b6849ac92ae234
xDark911 <faisalsh2002@hotmail.com>
2024-01-21 20:21:39 -0600

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-};
-
-module.exports = nextConfig
-
-export default nextConfig;
+    }
+    
+    module.exports = nextConfig
